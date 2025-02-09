12e29ee292c9bc94fc4c5bb7e5d6245cc47b9ce1
Sashvints <sashvints@gmail.com>
2025-02-10 02:22:53 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -45,14 +45,14 @@ function displayCat() {
     // Get the container where the image will be displayed
     var imageContainer = document.getElementById('image-container');
     // Create a new Image element for the cat
-    var catImage = new Image();
+    var bearImage = new Image();
     // Set the source (file path) for the cat image
-    catImage.src = 'displayCat'; // Assuming the cat image is named "cat.gif"
+    bearImage().src = 'displayCat'; // Assuming the cat image is named "cat.gif"
     // Set alternative text for the image (for accessibility)
-    catImage.alt = 'displayCatHeart';
+    bearImage()().alt = 'displayCatHeart';
     // When the cat image is fully loaded, add it to the image container
-    catImage.onload = function() {
-        imageContainer.appendChild(catImage);
+    bearImage()().onload = function() {
+        imageContainer.appendChild(bearImage()());
     };
 }
 
@@ -65,9 +65,9 @@ function displayCatHeart() {
     // Create a new Image element for the cat-heart
     var catHeartImage = new Image();
     // Set the source (file path) for the cat-heart image
-    catHeartImage.src = 'cat-heart.gif'; // Assuming the cat-heart image is named "cat-heart.gif"
+    catHeartImage.src = 'displaycatheart'; // Assuming the cat-heart image is named "cat-heart.gif"
     // Set alternative text for the image (for accessibility)
-    catHeartImage.alt = 'Cat Heart';
+    catHeartImage.alt = 'displayCat';
     // When the cat-heart image is fully loaded, add it to the image container
     catHeartImage.onload = function() {
         imageContainer.appendChild(catHeartImage);
